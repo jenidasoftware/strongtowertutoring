f730245b1fddc5f60426c191c8e473c32e6819f7
Julienne Arnold <julie.enid@gmail.com>
2014-12-11 19:20:47 -0600

    Add Controllers and Routes and update layout

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,32 @@
 <?php
 
-Route::resource('users', 'UsersController');
 
+Route::get('/', 'HomeController@showWelcome');
 Route::get('login', 'SessionsController@create');
 Route::get('logout', 'SessionsController@destroy');
 Route::resource('sessions', 'SessionsController');
 
+Route::resource('users', 'UsersController');
+Route::resource('profile', 'ProfileController');
+Route::resource('portfolio', 'PorfolioController');
+
+Route::resource('blog', 'BlogController');
+Route::resource('contact', 'ContactController');
+
+Route::resource('resources', 'ResourcesController');
+
+Route::resource('process', 'ProcessController');
+Route::resource('payment', 'PlansController');
+
+Route::resource('metacog', 'MetacogController');
+
+Route::resource('payment', 'PaymentController');
+
+Route::resource('shop', 'ShopController');
+
+Route::resource('blog', 'BlogController');
+
+Route::get('admin', function() 
+{
+	return 'Admin Page';
+})->before('auth');
